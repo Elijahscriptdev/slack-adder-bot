4be7c57b19d0a4c8876f12d20f587c6e7beedfdf
Elijahscriptdev <elijahobominuru@gmail.com>
2020-12-22 02:31:19 +0100

    remove white spaces

--- Gemfile
+++ Gemfile
@@ -1,25 +1,24 @@
-# frozen_string_literal: true
+source 'https://rubygems.org'
 
-source "https://rubygems.org"
-
-git_source(:github) {|repo_name| "https://github.com/#{repo_name}" }
+git_source(:github) { |repo_name| "https://github.com/#{repo_name}" }
 
 # gem "rails"
-gem 'slack-ruby-bot'
-gem 'puma'
-gem 'sinatra'
-gem 'dotenv'
 gem 'async-websocket', '~> 0.8.0'
 gem 'dentaku'
+gem 'dotenv'
+gem 'puma'
+gem 'rubocop'
+gem 'sinatra'
+gem 'slack-ruby-bot'
 
 group :development, :test do
-  gem 'rake'
   gem 'foreman'
+  gem 'rake'
 end
 
 group :test do
-  gem 'rspec'
   gem 'rack-test'
+  gem 'rspec'
   gem 'vcr'
   gem 'webmock'
 end
--- slack-mathbot/commands/calculate.rb
+++ slack-mathbot/commands/calculate.rb
@@ -1,19 +1,18 @@
 module SlackMathbot
-    module Commands
-      class Calculate < SlackRubyBot::Commands::Base
+  module Commands
+    class Calculate < SlackRubyBot::Commands::Base
+      addRegex = /[0-9]\+[0-9]/
 
-        addRegex = /[0-9]\+[0-9]/
+      match addRegex do |client, data, _match|
+        user_input = data.text
+        num_arr = user_input.split('+')
+        result = 0
+        num_arr.each do |item|
+          result += item.to_i
+        end
 
-        match addRegex do |client, data, match|
-            user_input = data.text
-            num_arr = user_input.split("+")
-            result = 0
-            num_arr.each do |item|
-                result = result + item.to_i
-            end
-
-            client.say(channel: data.channel, text: "#{result}")
-          end
+        client.say(channel: data.channel, text: result.to_s)
       end
     end
+  end
 end
